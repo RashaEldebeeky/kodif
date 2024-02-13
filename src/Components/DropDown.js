f7cb986d8b0791a78206644a758eb3cc6b7e7741
RashaEldebeeky <Rashaeldebeeky@gmail.com>
2024-02-13 12:38:25 -0800

    add dropdown component in json

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export default function DropDown({ data, style, styleActions }) {
+  const [value, setValue] = useState("");
   const name = data?.name;
   const options = data?.options;
   return (
-    <select name={name} id={name} style={{ ...style }} {...styleActions}>
+    <select
+      name={name}
+      id={name}
+      value={value}
+      style={{ ...style }}
+      {...styleActions}
+      onChange={(e) => setValue(e.target.value)}
+    >
+      <option disabled value="">
+        Select
+      </option>
       {options.map((option, idx) => (
         <option key={idx} value={option.value}>
           {option.label}
